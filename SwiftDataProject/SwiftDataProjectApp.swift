36992831734cbf662bc2fb1f5bd3d98a4427c462
Berserk-21 <cryptogekkofr@protonmail.com>
2024-06-21 11:39:55 +0200

    Initialize project

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Berserk on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
